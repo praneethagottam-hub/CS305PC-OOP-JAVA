fed54dac5538df2b5dfbcebf3c2e5c9190408268
praneethagottam-hub <praneethagottam@gmail.com>
2025-10-15 10:34:17 +0530

    Add files via upload

---
+++ Lab3P51.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Lab3P51{
+	public void demonstrate(){
+		System.out.println("Iam a method in Lab3P51");
+	}
+	public void setName(String s){
+		this.name=s;
+	}
+	public String getName(){
+		return this.name;
+		}
+		}
+		
+	
