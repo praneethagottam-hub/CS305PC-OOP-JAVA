ef9ce84ca87e6b78123d8970b227449846a14316
praneethagottam-hub <praneethagottam@gmail.com>
2025-10-22 11:59:52 +0530

    Add files via upload

---
+++ Added.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+	class Added{
+	public static void main(String...args){
+	Scanner S = new Scanner(System.in);
+		int a = S.nextInt();
+		int b = S.nextInt();
+		  int sum = a+b;
+		System.out.println("sum is:"+sum);
+		}
+		}
+		
+	
