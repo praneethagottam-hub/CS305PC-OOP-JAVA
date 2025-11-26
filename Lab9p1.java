aa37135155bc4df0544514a4fe1e07bd2ecdfd8a
praneethagottam-hub <praneethagottam@gmail.com>
2025-11-26 12:14:58 +0530

    Add files via upload

---
+++ Lab9p1.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Lab9p1 extends JFrame{
+Label l;
+
+	Lab9p1(){
+		super("Tabbed Pane Demo");
+		JTabbedPane jtp = new JTabbedPane();
+	        jtp.addTab("Tab1" ,new Label("TabbedPane 1"));
+	        jtp.addTab("Tab2" ,new Label("TabbedPane 2"));
+	        jtp.addTab("Tab3" ,new Label("TabbedPane 3"));
+		
+		add(jtp);
+		
+		setVisible(true);
+		setSize(500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public static void main(String...args){
+	new Lab9p1();
+	}
+    }
